e6a1f685fe7e4a808315d97298e0d1b7f2d72163
Bruno Honda <bruno.honda@live.com>
2023-04-02 15:33:49 -0300

    feat(app): convert on standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+  ]
 })
 export class AppComponent {
-  title = 'demo-standalone';
+  title = 'Demo Standalone';
 }
